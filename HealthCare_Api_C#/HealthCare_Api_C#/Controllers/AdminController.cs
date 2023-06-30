c28d5089fa6dfe6bde3ddadc5efdd9413763d353
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-07-01 01:04:14 +0530

    Three role controller Created

--- HealthCare_Api_C#/HealthCare_Api_C#/Controllers/AdminController.cs
+++ HealthCare_Api_C#/HealthCare_Api_C#/Controllers/AdminController.cs
@@ -1,6 +1,16 @@
 ﻿using HealthCare_Api_C_.Context;
+using HealthCare_Api_C_.Helpers;
+using HealthCare_Api_C_.Models;
+using HealthCare_Api_C_.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace HealthCare_Api_C_.Controllers
 {
@@ -8,7 +18,144 @@ namespace HealthCare_Api_C_.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-        private readonly JwtauthDbcontext dbcontext;
+        private readonly JwtauthDbcontext _authContext;
+
+        public AdminController (JwtauthDbcontext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] Admin userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Admins
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username);
+
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+
+            if (!PasswordHash.VerifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new { Message = "Password is Incorrect" });
+            }
+
+            user.Token = CreateJwt(user);
+            var newAccessToken = user.Token;
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(1);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new TokenApiDto()
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken
+            });
+        }
+        private string CreateJwt(Admin user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.Username}")
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddSeconds(10),
+                SigningCredentials = credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        private string CreateRefreshToken()
+        {
+            var tokenBytes = RandomNumberGenerator.GetBytes(64);
+            var refreshToken = Convert.ToBase64String(tokenBytes);
+
+            var tokenInUser = _authContext.Admins
+                .Any(a => a.RefreshToken == refreshToken);
+            if (tokenInUser)
+            {
+                return CreateRefreshToken();
+            }
+            return refreshToken;
+        }
+        private ClaimsPrincipal GetPrincipleFromExpiredToken(string token)
+        {
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("This is Invalid Token");
+            return principal;
+
+        }
+
+        [HttpGet("pending")]
+        public async Task<ActionResult<Doctor>> GetAllUsers()
+        {
+            return Ok(await _authContext.Doctors.ToListAsync());
+        }
+
+        [HttpGet("Approved")]
+        public async Task<ActionResult<Admin>> GetAllDoctors()
+        {
+            return Ok(await _authContext.Admins.ToListAsync());
+        }
+
+        /*[HttpPost]
+        public async Task<ActionResult<Admin>> InsertDoctor(Admin admin)
+        {
+            _authContext.Admins.Add(admin);
+            await _authContext.SaveChangesAsync();
+
+            return admin;
+        }*/
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody] TokenApiDto tokenApiDto)
+        {
+            if (tokenApiDto is null)
+                return BadRequest("Invalid Client Request");
+            string accessToken = tokenApiDto.AccessToken;
+            string refreshToken = tokenApiDto.RefreshToken;
+            var principal = GetPrincipleFromExpiredToken(accessToken);
+            var username = principal.Identity.Name;
+            var user = await _authContext.Admins.FirstOrDefaultAsync(u => u.Username == username);
+            if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+                return BadRequest("Invalid Request");
+            var newAccessToken = CreateJwt(user);
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            await _authContext.SaveChangesAsync();
+            return Ok(new TokenApiDto()
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken,
+            });
+        }
+
+
 
     }
 }
